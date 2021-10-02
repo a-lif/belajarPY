83506d10906f30f86a37bfa15ac50a60e9aae4b6
Alif Nurcahyo <63556067+a-lif@users.noreply.github.com>
2021-10-02 21:24:14 +0700

    Add files via upload

---
+++ 13_Latihan/main.py
@@ -0,0 +1,6 @@
+apel = 2
+harga = 5
+total = apel * harga
+
+print('Anda akan membeli ' + str(apel) + ' apel')
+print('Total harga adalah : ' + str(total) + ' dolar')
---
+++ 14_Input/main.py
@@ -0,0 +1,5 @@
+waifu = input('Siapakah waifu anda? ')
+
+# semua data yang diperoleh dari input() adalah string. Jika ingin diubah maka harus dilakukan konversi tipe data.
+print('Waifu anda adalah : ' + str(waifu))
+
